88fdeadcad14adb8f7ca256ddc18aacdc207e3c8
Frost <frost.github@proton.me>
2025-07-29 09:46:17 +0530

    zircon-miuicamera: Fix extract files
    The `.so` suffix was making it go brrrrr, also add `lib_fixups=lib_fixups`.
    
    Reformat extract-files.py via:
    `ruff format extract-files.py --config ../../../tools/extract-utils/ruff.toml`
    
    Change-Id: I3ba80febcc6fb8dc6cda11f326bbb175afed28cb
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -19,10 +19,8 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
-namespace_imports = [
-    'device/xiaomi/zircon-miuicamera',
-    'vendor/xiaomi/zircon'
-]
+namespace_imports = ['device/xiaomi/zircon-miuicamera', 'vendor/xiaomi/zircon']
+
 
 def lib_fixup_system_ext_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'system_ext' else None
@@ -30,7 +28,9 @@ def lib_fixup_system_ext_suffix(lib: str, partition: str, *args, **kwargs):
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    ('vendor.mediatek.hardware.camera.isphal@1.0.so',): lib_fixup_system_ext_suffix
+    (
+        'vendor.mediatek.hardware.camera.isphal@1.0',
+    ): lib_fixup_system_ext_suffix,
 }
 
 blob_fixups: blob_fixups_user_type = {
@@ -48,6 +48,7 @@ module = ExtractUtilsModule(
     'zircon-miuicamera',
     'xiaomi',
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
 
